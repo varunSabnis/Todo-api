d0c88064a7bc9fb85a3f97c66461774a9a401428
varunSabnis <varunsabnis@gmail.com>
2017-06-27 20:00:47 -0700

    Init

---
+++ server.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var app = express();
+var PORT = process.env.PORT || 3000;
+app.get('/', function(req,res){
+   res.send('TODO API root');
+});
+
+app.listen(PORT,function(){
+  console.log('App listening at port ' +PORT);
+});
