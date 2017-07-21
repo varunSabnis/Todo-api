80656d5b392db4eb68ca8b566e63c74e00f3f13d
varunSabnis <varunsabnis@gmail.com>
2017-07-21 10:33:36 -0700

    Add completed filter

--- server.js
+++ server.js
@@ -14,7 +14,19 @@ app.get('/', function(req,res){
 });
 
 app.get('/todos',function(req,res){
-	res.json(todos);
+    var queryparams = req.query;
+	var filteredtodos = todos;
+	if(queryparams.hasOwnProperty('completed') && queryparams.completed==='true')
+    {
+       filteredtodos = _.where(todos,{'completed' : true});
+
+    }
+    if(queryparams.hasOwnProperty('completed') && queryparams.completed==='false')
+    {
+    	filteredtodos = _.where(todos,{'completed': false});
+    }
+
+	res.json(filteredtodos);
 });
 app.get('/todos/:id',function(req,res){
 	
@@ -58,6 +70,39 @@ app.delete('/todos/:id',function(req,res){
 	    console.log('Successfully removed');
 	}
 });
+app.put('/todos/:id',function(req,res){
+var body = _.pick(req.body,'description','completed');
+var validAttributes = {};
+ var todoId = parseInt(req.params.id,10);
+var matchedTodo = _.findWhere(todos, {id: todoId});
+if(!matchedTodo)
+{
+  return res.status(404).json({"error":"no todo found with that id"});
+}
+ 
+  if(body.hasOwnProperty('completed') && _.isBoolean(body.completed))
+  {
+   validAttributes.completed=body.completed;
+   
+  }
+  else if(body.hasOwnProperty('completed')) 
+  {
+	res.status(400).send('Invalid Update');
+  }
+
+ if(body.hasOwnProperty('description')&& _.isString(body.description)&&body.description.trim().length >0)
+ {
+	validAttributes.description=body.description;
+
+ }
+ else if(body.hasOwnProperty('description'))
+ {
+  return res.status(400).send('Invalid update');	
+  }
+   matchedTodo = _.extend(matchedTodo, validAttributes);
+   res.json(matchedTodo);
+});
+
 app.listen(PORT,function(){
   console.log('App listening at port ' + PORT);
 });
